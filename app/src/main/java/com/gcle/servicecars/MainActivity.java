72179182d7543cb4ad9e9a228c16a0c10f661872
GerardoDLSJ <gerardo.delossantos19@sistemas.tecsanpedro.edu.mx>
2022-11-24 22:35:17 -0600

    Primer commit

---
+++ app/src/main/java/com/gcle/servicecars/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gcle.servicecars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Ocultar el action bar
+        getSupportActionBar().hide();
+    }
+}
